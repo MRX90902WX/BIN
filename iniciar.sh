1f9f3550b057301b8aa755b82eb8184a600978ad
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-03-01 17:16:13 -0500

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,30 +1,4 @@
 #! /bin/bash
-mkdir src
-
-echo "
-while :
-do
-echo -e '  \e[1;32mDesea generar un nuevo bin \e[0m'
-echo ''
-echo -n -e '  \e[1;32m(\e[0m\e[1;37msi\e[0m\e[1;32m)  (\e[0m\e[1;37mno\e[0m\e[1;32m) :\e[0m'
-read bin
-case $bin in
-si)
-#! /bash/bin
-cd /$HOME/BIN
-bash iniciar.sh
-exit
-;;
-no)
-#! /bash/bin
-cd /$HOME/BIN
-exit
-;;
-esac
-done
-" > received.sh
-mv received.sh src
-
 echo ""
 setterm -foreground green
 figlet BIN
