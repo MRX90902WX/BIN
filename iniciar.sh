112daf6f0552dbc85c135518df652bc4c1e08cb2
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-03-01 16:34:37 -0500

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,33 +1,28 @@
 #! /bin/bash
 mkdir src
+
 echo "
-echo ''
-setterm -foreground green
-figlet BIN
-echo ''
 while :
 do
-echo -e -n ' \e[1;32m1)\e[0m\e[1;37mGenerar bin Rnd\e[0m      \e[1;32m2)\e[1;37mGenerar bin con fecha Rnd\e[0m'
+echo -e '  \e[1;32mDesea generar un nuevo bin \e[0m'
 echo ''
-echo ''
-echo -n -e ' \e[1;32mOpción:\e[0m \e[1;37m\e[0m'
+echo -n -e '  \e[1;32m(\e[0m\e[1;37msi\e[0m\e[1;32m)  (\e[0m\e[1;37mno\e[0m\e[1;32m) :\e[0m'
 read bin
 case $bin in
-1)
-#! /bin/bash
-echo ''
-python bingnd.py
+si)
+#! /bash/bin
+cd /$HOME/BIN
+bash iniciar.sh
 exit
 ;;
-2)
-#! /bin/bash
-echo ''
-python binfecha.py
+no)
+#! /bash/bin
+cd /$HOME/BIN
 exit
 ;;
 esac
 done
-" > cd src/iniciar.sh
+" > cd src/received.sh
 
 echo ""
 setterm -foreground green
